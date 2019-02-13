08874bfdb4c0d65c7c455fe5bce92c7c07103f31
Peter Wilkins <peter.wilkins@polecat.com>
2019-02-13 19:18:44 +0000

    pong cljs with injellij support

---
+++ project.clj
@@ -0,0 +1,33 @@
+(defproject pong "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [quil "2.8.0"]
+                 [org.clojure/clojurescript "1.10.439"]
+                 [figwheel-sidecar "0.5.15"]]
+
+  :plugins [[lein-cljsbuild "1.1.7"]]
+  :hooks [leiningen.cljsbuild]
+
+  :clean-targets ^{:protect false} ["resources/public/js"]
+  :cljsbuild
+  {:builds [; development build with figwheel hot swap
+            {:id "development"
+             :source-paths ["src"]
+             :figwheel true
+             :compiler
+             {:main "pong.core"
+              :output-to "resources/public/js/main.js"
+              :output-dir "resources/public/js/development"
+              :asset-path "js/development"}}
+            ; minified and bundled build for deployment
+            {:id "optimized"
+             :source-paths ["src" "scripts"]
+             :compiler
+             {:main "pong.core"
+              :output-to "resources/public/js/main.js"
+              :output-dir "resources/public/js/optimized"
+              :asset-path "js/optimized"
+              :optimizations :advanced}}]})
